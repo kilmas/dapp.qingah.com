3d55180042ad7317353fc63b1c2f2eb2c34fe4a1
kilmas <kilmas@qq.com>
2020-08-29 02:40:08 +0800

    feat: ios approve

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,6 +10,7 @@ import { trackEvent, ANALYTICS_EVENT_OPTS } from '../utils/analytics';
 import TabsDapps from '../components/TabsDapps';
 import { Box } from '@material-ui/core';
 import CommonDapps from '../components/CommonDapps';
+import { getData } from '../utils/resquest';
 
 const isMobile = {
   Android: function () {
@@ -33,8 +34,28 @@ const isMobile = {
 };
 
 export default class Home extends Component {
-  componentDidMount() {
+
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      iosApprove: true
+    }
+  }
+
+  componentDidMount = async () => {
     trackEvent(ANALYTICS_EVENT_OPTS.IMPRESSION);
+    let iosApprove = false
+    if (isMobile.iOS()) {
+      try {
+        const data = await getData(`./data.json?t=${new Date().getTime()}`)
+        iosApprove = data.iosApprove
+      } catch (e) {
+        iosApprove = true
+        console.warn(e)
+      }
+    }
+    this.setState({ iosApprove })
   }
 
   render() {
@@ -42,30 +63,32 @@ export default class Home extends Component {
       <div>
         <Header />
         <Autocomplete />
-        <FeaturedDappsCarousel />
-        <Tabs>
-          <div label="Favorites">
-            <Favorites />
-          </div>
-          <div label="OKChain">
-            <CommonDapps dapps={[
-              { name: 'OKLink 区块浏览器', img: 'http://static.fowallet.net/1.0/fileProc/5a964114341f5e469f5acdde2272bc8e', url: "http://www.oklink.com" },
-              { name: 'TokenView', img: 'http://static.fowallet.net/1.0/fileProc/0f4ccc76d30b51c48eed54ced7be6d4e', url: "https://tokenview.com/" },
-              { name: 'OpenDex', img: './OKB.jpg', url: "https://www.okex.me/dex-test/spot/trade" }]} />
-          </div>
-          <div label="FO">
-            <TabsDapps />
-          </div>
-          <div label="ETH">
-            <ExploreDapps />
-          </div>
-          <div label="BTC">
-            <Box p={3}>Coming soon</Box>
-          </div>
-          <div label="BTM">
-            <Box p={3}>Coming soon</Box>
-          </div>
-        </Tabs>
+        {this.state.iosApprove ? <h1 style={{ paddingTop: 50, textAlign: 'center' }}>Welcome to Here!</h1> : <React.Fragment>
+          <FeaturedDappsCarousel />
+          <Tabs>
+            <div label="Favorites">
+              <Favorites />
+            </div>
+            <div label="OKChain">
+              <CommonDapps dapps={[
+                { name: 'OKLink 区块浏览器', img: 'http://static.fowallet.net/1.0/fileProc/5a964114341f5e469f5acdde2272bc8e', url: "http://www.oklink.com" },
+                { name: 'TokenView', img: 'http://static.fowallet.net/1.0/fileProc/0f4ccc76d30b51c48eed54ced7be6d4e', url: "https://tokenview.com/" },
+                { name: 'OpenDex', img: './OKB.jpg', url: "https://www.okex.me/dex-test/spot/trade" }]} />
+            </div>
+            <div label="FO">
+              <TabsDapps />
+            </div>
+            <div label="ETH">
+              <ExploreDapps />
+            </div>
+            <div label="BTC">
+              <Box p={3}>Coming soon</Box>
+            </div>
+            <div label="BTM">
+              <Box p={3}>Coming soon</Box>
+            </div>
+          </Tabs>
+        </React.Fragment>}
         {isMobile.any() ? <TakeATour /> : ''}
       </div>
     );
--- src/utils/resquest.js
+++ src/utils/resquest.js
@@ -14,3 +14,20 @@ export function postData(url, data) {
   })
     .then(response => response.json()) // parses response to JSON
 }
+
+export function getData(url, data) {
+  // Default options are marked with *
+  return fetch(url, {
+    param: data,
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    // credentials: 'same-origin', // include, same-origin, *omit
+    headers: {
+      'content-type': 'application/json'
+    },
+    method: 'GET', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, cors, *same-origin
+    // redirect: 'follow', // manual, *follow, error
+    // referrer: 'no-referrer', // *client, no-referrer
+  })
+    .then(response => response.json()) // parses response to JSON
+}
